95c3d5157de162145bcf49ff651080f1455b5a8b
Bo Lu <lv.patrick@gmail.com>
2019-05-22 21:54:27 +1000

    [publish binary]

--- .travis.sh
+++ .travis.sh
@@ -2,6 +2,21 @@
 
 set -ev
 
+sodium=libsodium-1.0.17
+base_dir=$TRAVIS_BUILD_DIR/libsodium
+sodium_dir=$base_dir/$sodium
+
+# Install libsodium
+mkdir -p $base_dir
+if [ ! -f $sodium_dir/Makefile  ]; then
+    wget https://download.libsodium.org/libsodium/releases/$sodium.tar.gz
+    tar -xzf $sodium.tar.gz -C $base_dir
+    cd $sodium_dir && ./configure --prefix=/usr && make
+fi
+cd $sodium_dir && sudo make install
+export SODIUM_LIB_DIR=/usr/lib
+export SODIUM_STATIC=true
+
 # Install Rust and Cargo
 curl https://sh.rustup.rs -sSf > /tmp/rustup.sh
 sh /tmp/rustup.sh -y
