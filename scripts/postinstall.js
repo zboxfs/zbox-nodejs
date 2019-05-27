f325cbdb4b00d229828054e3075935a932624528
Bo Lu <lv.patrick@gmail.com>
2019-05-27 16:19:13 +1000

    fix path resolve bug in post install script

--- scripts/postinstall.js
+++ scripts/postinstall.js
@@ -1,11 +1,12 @@
 const fs = require('fs');
+const path = require('path');
 
-const path = './node_modules/@zbox/nodejs/native/';
+const npath = path.join(__dirname, '..', 'node_modules/@zbox/nodejs/native/');
 
 try {
-  fs.readdirSync(path)
+  fs.readdirSync(npath)
     .filter(f => /[.]js$/.test(f))
-    .map(f => fs.unlinkSync(path + f));
+    .map(f => fs.unlinkSync(npath + f));
 } catch (err) {
   // ignore errors
 }
